dc453ba87c9541948e4cec1470d4ffd8b4e6ddbe
gwhitney15 <gwhitney115@gmail.com>
2022-07-12 21:05:14 -0500

    Grant

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+var tableRow = document.createElement(`tr`);
+
+Object.values(answersToRender).forEach(function (choice, index) {
+    var tableDetail = document.createElement("td");
+})
+
+var timeLeft = dueDate - now
+
+var dailyWage = hourlyWage * 8
+dailyWage * timeLeft
+
