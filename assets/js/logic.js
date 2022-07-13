1878f502433d1b2f7d758897c92071df9390942d
Johnathan <johnathan.mnn@gmail.com>
2022-07-12 21:21:45 -0500

    Clocks working -Johnny

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,5 @@
-var now = moment();
-$("#clock").text(now);
+var update = function() {
+    document.getElementById("datetime")
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(update, 1000);
